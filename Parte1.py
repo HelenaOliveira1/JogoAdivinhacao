7eb36fc5aeab535a5a22eb59f070d6d4c73a14a4
HelenaOliveira1 <helenna54319@gmail.com>
2016-11-08 22:21:33 -0200

    Parte1
    Jogo da Adivinhação simples, onde o PC pega um número aleatório e o
    usuário tem três chances para no chute adivinhar que número é esse.

---
+++ Parte1.py
@@ -0,0 +1,54 @@
+# Jogo da Adivinhação - Parte1
+
+num = 0
+
+def JogoAdivinhacao(num):
+
+    import random
+
+    num = random.randint(1,100)
+    numUsuario = int(input("Digite um número entre 1 e 100: "))
+    aux = 1
+    for i in range(1,3):
+        if (num == numUsuario):
+            print("Parabéns! Você venceu!")
+            break
+        elif (num > numUsuario):
+            print("Ops, o número é maior que este.")
+        elif (num < numUsuario):
+            print("Ops, o número é menor que este.")
+
+        numUsuario = int(input("Digite outro número entre 1 e 100: "))
+
+        aux += 1
+        if (aux == 3):
+            print("")
+            print("GAME OVER")
+
+            print("")
+            print("Digite 1 para JOGAR NOVAMENTE ou 2 para SAIR")
+            opc = int(input(""))
+            if (opc == 1):
+                JogoAdivinhacao(num)
+            else:
+                print("Jogo Encerrado!!")
+
+def menu():
+    print("Digite 1 para JOGAR ou 2 para INSTRUÇÕES")
+    opcPrincipal = int(input(""))
+    if (opcPrincipal == 1):
+        JogoAdivinhacao(num)
+    else:
+        print("Olá, Bem vindo ao nosso jogo da Adinhação.")
+        print("O jogo funciona assim, um número é aleatoriamente sorteado entre 1 e 100, e você vai ter que adivinhar qual é,")
+        print("Você terá apenas 3 chances.")
+        print("BOA SORTE!!")
+
+        print("")
+        print("Digite 1 para JOGAR ou 2 para SAIR")
+        opcInstrucoes = int(input(""))
+        if (opcInstrucoes == 1):
+            JogoAdivinhacao(num)
+        else:
+            print("Jogo Encerrado!!")
+menu()
